600707030078da9c84f7de6bb59f704b30fa713b
Manju31051996 <bbelagavi6@gmail.com>
2023-01-03 00:07:40 +0530

    [Manjunath S B] Day-7 :Tic-Tac-Toe Game :UC2 added the choose letter by user feature

--- src/com/bridgelabz_tic_tac_toe_game/TicTacToe.java
+++ src/com/bridgelabz_tic_tac_toe_game/TicTacToe.java
@@ -1,11 +1,16 @@
 package com.bridgelabz_tic_tac_toe_game;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     static char[] board = new char[10];
+    static char userLetter;
+    static char computerLetter;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe game....");
         createEmptyBoard();
+        chooseLetter();
     }
 
     /**
@@ -16,5 +21,16 @@ public class TicTacToe {
         for (int index = 1; index < board.length; index++) {
             board[index] = ' ';
         }
+    }
+
+    /**
+     * UC2
+     * Method Name: chooseLetter
+     */
+    private static void chooseLetter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose a letter :: X or O : ");
+        userLetter = scanner.next().toUpperCase().charAt(0);
+        computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
 }
