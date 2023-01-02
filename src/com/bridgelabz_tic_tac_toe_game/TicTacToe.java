7e6cd348742a391cd6ac2e571179779de751ff5b
Manju31051996 <bbelagavi6@gmail.com>
2023-01-03 00:18:17 +0530

    [Manjunath S B] Day-7 :Tic-Tac-Toe Game :UC3 display board to user,show board feature is added

--- src/com/bridgelabz_tic_tac_toe_game/TicTacToe.java
+++ src/com/bridgelabz_tic_tac_toe_game/TicTacToe.java
@@ -11,6 +11,7 @@ public class TicTacToe {
         System.out.println("Welcome to Tic-Tac-Toe game....");
         createEmptyBoard();
         chooseLetter();
+        showBoard();
     }
 
     /**
@@ -33,4 +34,17 @@ public class TicTacToe {
         userLetter = scanner.next().toUpperCase().charAt(0);
         computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
+
+    /**
+     * UC3
+     * Method Name: showBoard
+     */
+    private static void showBoard() {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("----------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("----------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
+
 }
