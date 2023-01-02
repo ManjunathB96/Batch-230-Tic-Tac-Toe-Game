ea10a83cb09e403187454a37e0b5b1b4be0171fd
Manju31051996 <bbelagavi6@gmail.com>
2023-01-03 00:02:05 +0530

    [Manjunath S B] Day-7 :Tic-Tac-Toe Game :UC1 created empty board

--- src/com/bridgelabz_tic_tac_toe_game/TicTacToe.java
+++ src/com/bridgelabz_tic_tac_toe_game/TicTacToe.java
@@ -1,8 +1,20 @@
 package com.bridgelabz_tic_tac_toe_game;
 
 public class TicTacToe {
+    static char[] board = new char[10];
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic-Tac-Toe game....");
+        createEmptyBoard();
+    }
 
+    /**
+     * UC1
+     * Method Name: createEmptyBoard
+     */
+    private static void createEmptyBoard() {
+        for (int index = 1; index < board.length; index++) {
+            board[index] = ' ';
+        }
     }
 }
